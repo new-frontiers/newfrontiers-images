ca53b8311944f3c11e3c87b0eecd38cc6a1c6144
markus_mueller <mueller@new-frontiers.de>
2016-11-09 09:30:21 +0100

    Added @covers annotations

--- tests/ImageTest.php
+++ tests/ImageTest.php
@@ -8,12 +8,19 @@ namespace NewFrontiers\Images;
 class ImageTest extends \PHPUnit_Framework_TestCase
 {
 
+    /**
+     * @covers Image::fromFile
+     */
     public function testFromFileNonExisting()
     {
         $this->expectException(\InvalidArgumentException::class);
         $image = Image::fromFile(__DIR__ . '/appdsdas.png');
     }
 
+    /**
+     * @covers Image::getWidth
+     * @covers Image::getHeight
+     */
     public function testGetDimension()
     {
         $image = Image::fromFile(__DIR__ . '/app.png');
@@ -22,6 +29,9 @@ class ImageTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(256, $image->getHeight());
     }
 
+    /**
+     * @covers Image::resizeTo
+     */
     public function testResize()
     {
         $image = Image::fromFile(__DIR__ . '/app.png');
@@ -32,6 +42,9 @@ class ImageTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(512, $newImage->getHeight());
     }
 
+    /**
+     * @covers Image::resizeToMax
+     */
     public function testResizeToMax()
     {
         $image = Image::fromFile(__DIR__ . '/a9f54a31915697.5666acd712a3c.jpg');
